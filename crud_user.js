cd1fedafc9d7f1bd04b764dee8c478e276663318
git <bhargav.kanodiya@bacancy.com>
2021-01-10 16:26:22 +0530

    task completed

--- crud_user.js
+++ crud_user.js
@@ -3,11 +3,10 @@ const sequelize = require('./connection');
 const User = require('./user');
 
 async function addUser(){
-const user =User.create({id:"4", name: "shane" , age: "24",});
-// user exists in the database now!
-console.log(user.name); 
-console.log('User is added to the database!');
-(await user).save
+    const user =User.create({id:"4", name: "shane" , age: "24",});
+    console.log(user.name); 
+    console.log('User is added to the database!');
+    (await user).save
 }
 async function updateUser(){
     console.log(sequelize.models);
@@ -23,13 +22,13 @@ async function getUser(){
         console.log(" id: "+dataValues.id+" name: "+ dataValues.name +" age: "+dataValues.age );
     });
     
-    // console.log(" id: "+user.id+" name: "+user.name+" age: "+user.age );
+
 }
 async function deleteUser(){
     const user = await User.findOne({id:1});
     await user.destroy();
     console.log("user deleted..!")
 }
-// the name is still "Jane" in the database
+
 
 module.exports= {addUser,updateUser,getUser,deleteUser};
